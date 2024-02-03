dafe3f227bb6b86641041869cfd5927588d65def
TheMauss <142694330+TheMauss@users.noreply.github.com>
2024-02-03 20:54:22 +0100

    Revert "RecentTrades"
    This reverts commit ea17c0914469001b2c99de29796568e2bbd4d6a8.

--- src/components/RecentPredictionsNew.tsx
+++ src/components/RecentPredictionsNew.tsx
@@ -28,8 +28,6 @@ interface Position {
   payout: number;
   finalPrice: number;
   elapsedTime: string;
-  timestamp: number;
-
 }
 
 interface PositionFutures {
@@ -158,7 +156,7 @@ const RecentPredictions: FC<Recentprops> = ({ divHeight }) => {
     });
 
     socket.on("latestPositions", (newPositions: Position[]) => {
-      handleNewPositions(newPositions, "timestamp");
+      handleNewPositions(newPositions, "expirationTime");
     });
 
     socket.on("newPosition", (newPosition: Position) => {
@@ -185,7 +183,7 @@ const RecentPredictions: FC<Recentprops> = ({ divHeight }) => {
         ];
 
         // Limit the length of the positions array to 100
-        if (newPositions.length > 40) {
+        if (newPositions.length > 15) {
           newPositions.pop(); // Remove the last element of the array (oldest position)
         }
 
@@ -202,7 +200,7 @@ const RecentPredictions: FC<Recentprops> = ({ divHeight }) => {
         const newPositions = [{ ...newPosition }, ...prevPositions];
 
         // Limit the length of the positions array to 100
-        if (newPositions.length > 40) {
+        if (newPositions.length > 15) {
           newPositions.pop(); // Remove the last element of the array (oldest position)
         }
 
@@ -221,14 +219,60 @@ const RecentPredictions: FC<Recentprops> = ({ divHeight }) => {
     };
   }, []);
 
-  
-  
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setPositions((prevPositions) => {
+        if (Array.isArray(prevPositions)) {
+          return prevPositions.map((pos) => {
+            const currentTime = Math.floor(Date.now() / 1000);
+            let timeValue: number;
+
+            if ("expirationTime" in pos) {
+              timeValue = pos.expirationTime;
+            } else if ("timestamp" in pos) {
+              timeValue = pos.timestamp;
+            } else {
+              console.error("Invalid position object:", pos);
+              return pos;
+            }
+
+            const timeElapsed = currentTime - timeValue;
+
+            const seconds = Math.floor(timeElapsed);
+            const minutes = Math.floor(timeElapsed / 60);
+            const hours = Math.floor(minutes / 60);
+
+            let elapsedTime = "";
+            if (hours > 0) {
+              elapsedTime = `${hours} ${hours > 1 ? "hours" : "hour"} ago`;
+            } else if (minutes > 0) {
+              elapsedTime = `${minutes} ${minutes > 1 ? "minutes" : "minute"} ago`;
+            } else {
+              elapsedTime = `${seconds} ${seconds > 1 ? "seconds" : "second"} ago`;
+            }
+
+            return {
+              ...pos,
+              elapsedTime,
+            };
+          });
+        } else {
+          console.error("Invalid previous positions:", prevPositions);
+          return prevPositions;
+        }
+      });
+    }, 15000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
 
   return (
-    <div className="font-poppins w-full flex flex-col overflow-hidden h-full md:overflow-y-scroll  custom-scrollbar  order-5 xl:order-3 lg:order-3 md:order-5 rounded-lg bg-layer-1 py-4  flex bg-layer-1">
+    <div className="font-poppins w-full flex flex-col overflow-hidden h-full md:overflow-y-scroll  custom-scrollbar  order-5 xl:order-3 lg:order-3 md:order-5 rounded-lg bg-layer-1 p-4  flex bg-layer-1">
       <div className="sticky top-0 flex items-center z-10">
-        <h2 className="bankGothic leading-[18px] text-xl text-start text-grey-text px-4">
-          Recent Trades
+        <h2 className="bankGothic leading-[18px] text-xl text-start text-grey-text">
+          Recent Predictions
         </h2>
       </div>
 
@@ -238,18 +282,19 @@ const RecentPredictions: FC<Recentprops> = ({ divHeight }) => {
         </div>
       ) : (
         <div className="custom-scrollbar flex flex-col overflow-auto ">
-          <div className="sticky top-0 bg-layer-1 flex z-10 pt-2 px-4">
-            <div className="w-[38%] text-sm leading-[12px] text-grey-text py-2 ">
-              Pair
+          <div className="sticky top-0 bg-layer-1 flex z-10 pt-2">
+            <div className="w-[25%] text-sm leading-[12px] text-grey-text py-2 ">
+              User
             </div>
-            <div className="w-[30%] text-sm leading-[12px] text-grey-text py-2 text-end">
-              Price
+            <div className="w-[27%] text-sm leading-[12px] text-grey-text py-2"></div>
+            <div className="w-[25%] text-sm leading-[12px] text-grey-text py-2">
+              Lvg
             </div>
-            <div className="w-[23%] text-sm leading-[12px] text-grey-text py-2 text-end">
-              Size ◎
+            <div className="w-[25%] text-sm leading-[12px] text-grey-text py-2">
+              PnL
             </div>
-            <div className="w-[20%] text-sm leading-[12px] text-grey-text py-2 text-end">
-              Time
+            <div className="w-[17%] text-sm leading-[12px] text-grey-text py-2">
+              ROI
             </div>
           </div>
           <FlipMove>
@@ -292,145 +337,116 @@ const RecentPredictions: FC<Recentprops> = ({ divHeight }) => {
                     : (item.pnl / item.betAmount) * 100;
                 percentage = Number(percentage);
 
-                const positionSize = "binaryOption" in item 
-                    ? item.betAmount / LAMPORTS_PER_SOL
-                    : item.leverage * item.betAmount / LAMPORTS_PER_SOL;
-                    
-                const minPositionSize = 0;
-                const maxPositionSize = 500; // The value corresponding to 100% green
-                let greenPercentage = ((positionSize - minPositionSize) / (maxPositionSize - minPositionSize)) * 100;
-                    
-                // Define a minimum visible percentage for small bet amounts
-                const minVisiblePercentage = 2; // for example, 5%
-                    
-                // Adjust greenPercentage to be at least the minimum visible percentage if it's too small but greater than zero
-                if (greenPercentage > 0 && greenPercentage < minVisiblePercentage) {
-                    greenPercentage = minVisiblePercentage;
-                    }                
-                const colorfill = Math.max(0, Math.min(greenPercentage, 100));
-
                 return (
-                  
                   <div
                     key={item._id}
-                    className={`flex text-sm my-1 w-full pr-4 pl-3 hover:bg-layer-3`}
-                    style={{
-                      background: `linear-gradient(to right, transparent ${100 - colorfill}%, ${
-                        item.priceDirection === 1
-                          ? (item.resolved ? "rgba(52, 199, 150, 0.2)" : "rgba(255, 76, 76, 0.2)")
-                          : (item.resolved ? "rgba(255, 76, 76, 0.2)" : "rgba(52, 199, 150, 0.2)")
-                      } ${100 - colorfill}%)`
-                    }}
+                    className={`shadow-lg flex text-[0.9rem] rounded my-1.5  ${profit > 4.99 && profit > 0 ? "text-slate-300" : "text-slate-300"}`}
                   >
-                    <div className="w-[38%] leading-[12px] flex items-center">
+                    <div className="w-[40%] py-1">
                       <a
                         href={`https://solscan.io/account/${"binaryOption" in item ? item.playerAcc : item.playerAcc}`}
                         target="_blank"
                         rel="noreferrer"
                         className="hover:underline"
                       >
-                        <div className="flex flex-row items-center">
+                        <div className="flex items-center">
                           {item.symbol === 0 ? (
-                           <div className="py-0.5 flex flex-row justify-start">
-                              <img
-                                src="/coins/60x60/Sol.png"
-                                alt="Logo"
-                                width="20"
-                                height="16"
-                              />
-                              <p className="flex ml-1 items-center">SOL/USD</p>
-                            </div>
+                            <img
+                              src="/coins/60x60/Sol.png"
+                              alt="Logo"
+                              width="24"
+                              height="24"
+                              className="pb-1"
+                            />
                           ) : item.symbol === 1 ? (
-                           <div className="py-0.5 flex flex-row justify-start">
-                              <img
-                                src="/coins/60x60/Btc.png"
-                                alt="Logo"
-                                width="20"
-                                height="16"
-                              />
-                              <p className="flex ml-1 items-center">BTC/USD</p>
-                            </div>
+                            <img
+                              src="/coins/60x60/Btc.png"
+                              alt="Logo"
+                              width="24"
+                              height="24"
+                              className="pb-1"
+                            />
                           ) : item.symbol === 2 ? (
-                           <div className="py-0.5 flex flex-row justify-start">
-                              <img
-                                src="/coins/60x60/Pyth.png"
-                                alt="Logo"
-                                width="20"
-                                height="16"
-                              />
-                              <p className="flex ml-1 items-center">PYTH/USD</p>
-                            </div>
+                            <img
+                              src="/coins/60x60/Pyth.png"
+                              alt="Logo"
+                              width="24"
+                              height="24"
+                              className="pb-1"
+                            />
                           ) : item.symbol === 3 ? (
-                           <div className="py-0.5 flex flex-row justify-start">
-                              <img
-                                src="/coins/60x60/Bonk.png"
-                                alt="Logo"
-                                width="20"
-                                height="16"
-                              />
-                              <p className="flex ml-1 items-center">BONK/USD</p>
-                            </div>
+                            <img
+                              src="/coins/60x60/Bonk.png"
+                              alt="Logo"
+                              width="24"
+                              height="24"
+                              className="pb-1"
+                            />
                           ) : item.symbol === 4 ? (
-                           <div className="py-0.5 flex flex-row justify-start">
-                              <img
-                                src="/coins/60x60/Jup.png"
-                                alt="Logo"
-                                width="20"
-                                height="16"
-                              />
-                              <p className="flex ml-1 items-center">JUP/USD</p>
-                            </div>
+                            <img
+                              src="/coins/60x60/Jup.png"
+                              alt="Logo"
+                              width="24"
+                              height="24"
+                              className="pb-1"
+                            />
                           ) : item.symbol === 5 ? (
-                           <div className="py-0.5 flex flex-row justify-start">
-                              <img
-                                src="/coins/60x60/Eth.png"
-                                alt="Logo"
-                                width="20"
-                                height="16"
-                              />
-                              <p className="flex ml-1 items-center">ETH/USD</p>
-                            </div>
+                            <img
+                              src="/coins/60x60/Eth.png"
+                              alt="Logo"
+                              width="24"
+                              height="24"
+                              className="pb-1"
+                            />
                           ) : item.symbol === 6 ? (
-                           <div className="py-0.5 flex flex-row justify-start">
-                              <img
-                                src="/coins/60x60/Tia.png"
-                                alt="Logo"
-                                width="20"
-                                height="16"
-                              />
-                              <p className="flex ml-1 items-center">TIA/USD</p>
-                            </div>
+                            <img
+                              src="/coins/60x60/Tia.png"
+                              alt="Logo"
+                              width="24"
+                              height="24"
+                              className="pb-1"
+                            />
                           ) : item.symbol === 7 ? (
-                           <div className="py-0.5 flex flex-row justify-start">
-                              <img
-                                src="/coins/60x60/Sui.png"
-                                alt="Logo"
-                                width="20"
-                                height="16"
-                              />
-                              <p className="flex ml-1 items-center">SUI/USD</p>
-                            </div>
+                            <img
+                              src="/coins/60x60/Sui.png"
+                              alt="Logo"
+                              width="24"
+                              height="24"
+                              className="pb-1"
+                            />
                           ) : null}
+                          <p className="ml-2 text-grey-text">{`${"binaryOption" in item ? item.playerAcc.slice(0, 2) : item.playerAcc.slice(0, 2)}..${"binaryOption" in item ? item.playerAcc.slice(-2) : item.playerAcc.slice(-2)}`}</p>
                         </div>
                       </a>
                     </div>
-                    <div className={`w-[30%] leading-[12px] flex items-center justify-end ${
-                      item.priceDirection === 1
-                      ? (item.resolved ? "text-[#34c796]" : "text-red-500") // If priceDirection is 1, green if resolved, otherwise red
-                      : (item.resolved ? "text-red-500" : "text-[#34c796]") // If priceDirection is 0, red if resolved, otherwise green
-                      }`}>
-                      ${item.resolved ? (item.finalPrice/100000000).toFixed(1) : (item.initialPrice/100000000).toFixed(1)}
+                    <div className="w-[9%] flex justify-start items-start text-left">
+                      {item.priceDirection === 0 ? (
+                        <img
+                          className="relative w-8 h-8"
+                          alt=""
+                          src="/new/component-82.svg"
+                        />
+                      ) : (
+                        <img
+                          className="relative w-8 h-8"
+                          alt=""
+                          src="/new/component-81.svg"
+                        />
+                      )}
                     </div>
-                    
-                    <div className={`w-[23%] leading-[12px] flex items-center justify-end`}>
-                    
-                      {"binaryOption" in item ? `${(item.betAmount/LAMPORTS_PER_SOL).toFixed(1)}` : `${(item.leverage*item.betAmount/LAMPORTS_PER_SOL).toFixed(1)}`} 
+                    <div className="w-[25%] flex items-center text-white">
+                      {"binaryOption" in item ? "BIN" : `${item.leverage}X`}
                     </div>
-                    <div className={`w-[20%] leading-[12px] flex items-center justify-end text-grey-text`}>
-                    {
-                      new Date(item.timestamp*1000).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
-                    }
-                  </div>
+                    <div
+                      className={`w-[25%] flex items-center text-right ${value < 0 ? "negative text-red-500" : "text-[#34c796]"}`}
+                    >
+                      {value.toFixed(2)}
+                    </div>
+                    <div
+                      className={`w-[17%] flex items-center text-right pr-2 ${percentage < 0 ? "negative text-red-500" : "text-[#34c796]"}`}
+                    >
+                      {percentage.toFixed(0)}%
+                    </div>
                   </div>
                 );
               })}
