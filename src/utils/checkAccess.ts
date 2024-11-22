da3ad0bd9c524c653fd50828dff8339c43134f78
TheMauss <binary.finance@gmail.com>
2024-11-22 20:16:27 +0100

    newuser

--- src/utils/checkAccess.ts
+++ src/utils/checkAccess.ts
@@ -39,6 +39,7 @@ export async function hasAccess(publicKey: PublicKey | null): Promise<boolean> {
     "FCHdEP1XRgqc7eFqYzkRk2KW83okcyybqRE5rgS2UiM7",
     "fpdDL7DCWsVwVbTRwZBtzXuPFwzps4v2chFKyFQ41d2",
     "DHF7q82aW4g462xRKdwne92KwRJ5nG6P1FePCXPvSAiM",
+    "CgPSggQszWvP2Yx9KTinEaY8oof2FgWPEavKgR5eQJUP",
   ];
 
   return allowedPublicKeys.includes(publicKey.toBase58());
