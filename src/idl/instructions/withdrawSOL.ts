98cdb233e03d3d838f90a0a4cc6e7eaac4fa58b2
TheMauss <binary.finance@gmail.com>
2025-02-25 14:24:28 +0100

    pls

--- src/idl/instructions/withdrawSOL.ts
+++ src/idl/instructions/withdrawSOL.ts
@@ -78,7 +78,6 @@ export function withdraw(
       isSigner: false,
       isWritable: false,
     }, // Add account 18
-    { pubkey: accounts.acc18, isSigner: false, isWritable: false }, // Add account 20
     { pubkey: accounts.acc19, isSigner: false, isWritable: false }, // Add account 21
     { pubkey: accounts.acc21, isSigner: false, isWritable: false }, // Add account 20
     { pubkey: accounts.acc20, isSigner: false, isWritable: false }, // Add account 22
